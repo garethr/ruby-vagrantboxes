191a3de8d998d2d396f0ef71c44f74041a60495d
Gareth Rushgrove <gareth@morethanseven.net>
2011-05-08 14:22:59 +0100

    Added show method

--- lib/vagrantboxes/command.rb
+++ lib/vagrantboxes/command.rb
@@ -26,6 +26,23 @@ module Vagrantboxes
       }
     end
 
+    desc "show [ID]", "Show all information for a given box"
+    def show(id=nil)
+  
+      if id =~ /^[-+]?[0-9]+$/
+        url = "#{ENDPOINT}/boxes/#{id}.json"
+        resp = Net::HTTP.get_response(URI.parse(url))
+        if resp.code == "200"
+          data = resp.body
+          result = JSON.parse(data)
+          puts result['title']
+          puts result['url']
+          puts
+          puts result['description']
+        end
+      end
+    end
+
     desc "url [ID]", "Return the URL for a given box"
     def url(id=nil)
   
